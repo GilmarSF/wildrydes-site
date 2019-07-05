7cfcf39b91e35c57bd9937a06999cf14c43d9d8f
GilmarSF <GilmarSF@users.noreply.github.com>
2019-07-05 14:12:39 -0300

    Ateracao config cognito
    Ateracao config cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '4d8n9eprp4gqph0udad4o014h7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'arn:aws:cognito-idp:us-east-1:289461853952:userpool/us-east-1_z8qEJzdw5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
