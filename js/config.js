696b95d9e2be7bef031005969e143842be9cf570
GilmarSF <GilmarSF@users.noreply.github.com>
2019-07-05 14:17:53 -0300

    Alteracao config
    Alteracao config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: '4d8n9eprp4gqph0udad4o014h7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'arn:aws:cognito-idp:us-east-1:289461853952:userpool/us-east-1_z8qEJzdw5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4d8n9eprp4gqph0udad4o014h7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
